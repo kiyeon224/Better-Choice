92a48a638db9ab90aa9b769326fb3ba581c86a3a
kiyeon224 <kiyeon224@naver.com>
2019-05-07 13:26:40 +0900

    add 액티비티 완성

--- app/src/main/java/com/example/better_choice/AddActivity.java
+++ app/src/main/java/com/example/better_choice/AddActivity.java
@@ -8,10 +8,15 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.StateListAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +27,16 @@ public class AddActivity extends AppCompatActivity {
 
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back_afafaf_24dp);  //뒤로가기 icon을 ic_back_afafaf_24dp로 바꿔줌
 
-        // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+        /* 뒤로가기 버튼 */
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+
+        /* 고민의 유형 선택 (Spinner) */
+        Spinner staticSpinner = (Spinner) findViewById(R.id.static_spinner);
+        ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter.createFromResource(this, R.array.brew_array, android.R.layout.simple_spinner_item);
+
+        staticAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        staticSpinner.setAdapter(staticAdapter);
     }
 
     @Override
@@ -37,5 +49,6 @@ public class AddActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 
 }
